ea9fd43d22a7f4268018f5bc08568edf06e466e5
Rashid <rashid.best.ry@gmail.com>
2026-04-24 07:19:59 +0300

    edited shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,11 +1,6 @@
 sudo apt-get install cowsay -y
-
-    - name: Execute cowsay CMD
-      run:  cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-
-    - name: TEST file exists
-      run: grep -i "dragon" dragon.txt
-    
-    - name: REad file
-      run: cat dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
     
